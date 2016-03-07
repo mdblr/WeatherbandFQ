2f2dd0323a9d4f0dfc1dae30f43d519ec43af036
Max Dobler <mdblr@outlook.com>
2016-03-07 14:49:41 -0800

    console logging initial api call

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('sanity check!');
+$.ajax({
+  method: "GET",
+  url: "https://api.forecast.io/forecast/ec4ea27eb974f4bdcd500583b2c49367/47.6302,-122.3461"
+})
+.done(function(info) {
+    console.log("DONE")
+    console.log(info)
+  //write all my code that relies on the response data
+ })
+.fail(function(err){
+  console.log("FAIL")
+  console.log(err)
+});
