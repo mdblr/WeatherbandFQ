21a8cea17d8ee3c9f4515fb4b61418090920e2bc
Max Dobler <mdblr@outlook.com>
2016-03-08 13:44:04 -0800

    implement jquery cycle

--- app.js
+++ app.js
@@ -20,6 +20,17 @@ function displayForecast(info) {
 
 }
 
+
+$(document).ready(function() {
+	$('#slideshow').cycle({
+	fx: 'fade',
+	pager: '#smallnav',
+	pause:   1,
+	speed: 1000,
+	timeout:  3500
+	});
+});
+
 // HTML5 Geolocation
 
 // function userCoords() {
