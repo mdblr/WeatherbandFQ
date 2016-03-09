6b0ebf4f488b636bf86a099bd7bc08f60e85d6da
Max Dobler <mdblr@outlook.com>
2016-03-08 21:18:45 -0800

    larger hero text

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
 var $temp = $('#temp');
 var $summary = $('#summary');
-var today = moment().format("[today] hA");;
+var today = moment().format("[today] h:mm a");;
 
 
 
 // Dashboard
 function displayForecast(info) {
-    // var temp = Math.floor(info.hourly.data[0].temperature) + '\u00B0';
-    // $temp.append(temp);
-    // // $test.append(info.currently.icon);
-    // $summary.append('<div>'+ today +'</div>');
-    // $summary.append('<div>'+ info.hourly.data[0].summary +'</div>');
-
-    var temp = "47" + '\u00B0';
+    var temp = Math.floor(info.hourly.data[0].temperature) + '\u00B0';
     $temp.append(temp);
     // $test.append(info.currently.icon);
     $summary.append('<div>'+ today +'</div>');
-    $summary.append('<div> It"s cold out!</div>');
+    $summary.append('<div>'+ info.hourly.data[0].summary +'</div>');
+
+    // static code for style testing
+    // var temp = "47" + '\u00B0';
+    // $temp.append(temp);
+    // $summary.append('<div>'+ today +'</div>');
+    // $summary.append('<div> It"s cold out!</div>');
+
+    // $test.append(info.currently.icon);
+
 
 }
 
@@ -31,49 +34,52 @@ $(document).ready(function() {
 	});
 });
 
+// Forecast.io call
+
+
+
 
 // HTML5 Geolocation
 
-// function userCoords() {
-//   if (!navigator.geolocation) {
-//     $temp.append('<div> Please enable access to your location or switch to a modern browser</div>');
-//     return;
-//   }
-//
-//   function success(position) {
-//     var latitude = position.coords.latitude;
-//     var longitude = position.coords.longitude;
-//
-//     displayForecast(info);
+function userCoords() {
+  if (!navigator.geolocation) {
+    $temp.append('<div> Please enable access to your location or switch to a modern browser</div>');
+    return;
+  }
 
-    // Forecast.io call
-    // $.ajax({
-    //   method: "GET",
-    //   dataType: 'jsonp',
-    //   url: "https://api.forecast.io/forecast/ec4ea27eb974f4bdcd500583b2c49367/" +
-    //   latitude + "," + longitude
-    // })
-    // .done(function(info) {
-    //     console.log("DONE");
-    //     console.log(info);
-    //   //write all my code that relies on the response data
-    //     displayForecast(info);
-    //  })
-    // .fail(function(err){
-    //   console.log("FAIL");
-    //   console.log(err);
-    // });
+  function success(position) {
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
 
-//   };
-//
-//   function error(info){
-//     console.log(info);
-//     $temp.append('<div> Something went wrong ): </div>');
-//   };
-//
-//   navigator.geolocation.getCurrentPosition(success, error);
-// }
-//
-// userCoords();
+    $.ajax({
+      method: "GET",
+      dataType: 'jsonp',
+      url: "https://api.forecast.io/forecast/ec4ea27eb974f4bdcd500583b2c49367/" +
+      latitude + "," + longitude
+    })
+    .done(function(info) {
+        console.log("DONE");
+        console.log(info);
+      //write all my code that relies on the response data
+        displayForecast(info);
+     })
+    .fail(function(err){
+      console.log("FAIL");
+      console.log(err);
+    });
+
+    displayForecast(info);
+
+  };
+
+  function error(info){
+    console.log(info);
+    $temp.append('<div> Something went wrong ): </div>');
+  };
+
+  navigator.geolocation.getCurrentPosition(success, error);
+}
+
+userCoords();
 
 displayForecast();
