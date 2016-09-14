a35fbf62c3009d4f09da18f5f29a9d5c46c51aa0
Max Dobler <mdblr@outlook.com>
2016-09-14 13:31:38 -0700

    move listener event code into separate functions

--- menu/app/app.js
+++ menu/app/app.js
@@ -1,76 +1,68 @@
 let vis = true;
-const $inputs = $('input');
-const $menu = $('#menu');
 let $form = $('form');
-const $bars = $('.bar');
 let $submit = $('#click');
 
+//when menu is open listen to $submit
+$submit.click(e => {
 
-$submit.click( (e) => {
+  menu(vis, $form);
 
-  vis ? hide($menu, $form) : show($menu, $form);
+  if (vis) {
+    closedListener(e, $form);
+  };
+
+  vis = !vis;
+});
+
+/// functions menu, closedListener, manPane, manForm
+
+function menu(vis, form) {
+  manPane(vis);
+  manForm(vis, form);
+};
+
+function closedListener(e, $form) {
+  e.stopPropagation();
+  $($form).on('click', () => {
+    menu(vis, $form);
+    vis = !vis;
+    $form.off('click');
+  })
+}
+
+function manPane(vis) {
+  const $menu = $('#menu');
+
+  if (vis) {
+    $menu.animate({'height': '8vh'});
+    $menu.removeClass('init');
+  }
+  else {
+    $menu.animate({'height': '80vh'});
+  }
+};
+
+function manForm(vis, form) {
+  const $inputs = $('input'),
+        $bars = $('.bar'),
+        $submit = $('#click');
+
+  if (vis) {
+    let left_c = $('body').width()/2 - (form.width()/5.35714286)/2;
+    form.animate({left: ~left_c });
+  } else {
+    form.animate({left: 0});
+  }
 
   for (var i = 0; i < $inputs.length; i++) {
-    vis ? $inputs.eq(i).slideUp() : $inputs.eq(i).slideDown();
-    vis ? $bars.eq(i).animate({'width':'2.8vw'}) : $bars.eq(i).animate({'width': '15vw'});
+    vis ?
+    $inputs.eq(i).slideUp():
+    $inputs.eq(i).slideDown();
+    vis ?
+    $bars.eq(i).animate({'width':'2.8vw'}):
+    $bars.eq(i).animate({'width': '15vw'});
   }
 
   vis ? $submit.slideUp() : $submit.slideDown();
 
-  if (vis) {
-    e.stopPropagation();
-
-    $($form).on('click', () => {
-      $form.off('click');
-      vis ? hide($menu, $form) : show($menu, $form);
-      for (var i = 0; i < $inputs.length; i++) {
-        vis ? $inputs.eq(i).slideUp() : $inputs.eq(i).slideDown();
-        vis ? $bars.eq(i).animate({'width':'2.8vw'}) : $bars.eq(i).animate({'width': '15vw'});
-      }
-      vis ? $submit.slideUp() : $submit.slideDown();
-      vis = !vis;
-    })
-  };
-
-
-  vis = !vis;
-
-});
-
-
-
-function hide(menu, form) {
-  let left_c = $('body').width()/2 - (form.width()/5.35714286)/2;
-  menu.animate({'height': '8vh'});
-  menu.removeClass('init');
-  form.animate({left: ~left_c });
-}
-
-function show(menu, form) {
-  menu.animate({'height': '80vh'});
-  form.animate({left: 0});
-}
-
-// function toggleMenu(vis) {
-//   if (vis) {
-//     menu.animate({'height': '8vh'});
-//     menu.removeClass('init');
-//   }
-//   else {
-//     menu.animate({'height': '80vh'});
-//   }
-// }
-//
-// function toggleForm(vis) {
-//   if (vis) {
-//     let left_c = $('body').width()/2 - (form.width()/5.35714286)/2;
-//     form.animate({left: ~left_c });
-//     $inputs.eq(i).slideUp();
-//     $bars.eq(i).animate({'width':'2.8vw'});
-//   }
-//   else {
-//     form.animate({left: 0});
-//     $inputs.eq(i).slideDown();
-//     $bars.eq(i).animate({'width': '15vw'});
-//   }
-// }
+};
