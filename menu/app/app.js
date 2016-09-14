c6b631ae2ee1b6e0cd6436e117e2645267355a8d
Max Dobler <mdblr@outlook.com>
2016-09-14 15:33:27 -0700

    update form position u/m to vw

--- menu/app/app.js
+++ menu/app/app.js
@@ -1,68 +1,80 @@
-let vis = true;
-let $form = $('form');
-let $submit = $('#click');
+(function () {
+  let vis = true;
+  let $form = $('form');
+  let $submit = $('#click');
 
-//when menu is open listen to $submit
-$submit.click(e => {
+  //when menu is open listen to $submit
+  $submit.click(e => {
 
-  menu(vis, $form);
+    menu(vis, $form);
 
-  if (vis) {
-    closedListener(e, $form);
+    if (vis) {
+      closedListener(e, $form);
+    };
+
+    vis = !vis;
+  });
+
+  /// functions menu, closedListener, manPane, manForm
+
+  function menu(vis, form) {
+    manPane(vis);
+    manForm(vis, form);
   };
 
-  vis = !vis;
-});
-
-/// functions menu, closedListener, manPane, manForm
-
-function menu(vis, form) {
-  manPane(vis);
-  manForm(vis, form);
-};
-
-function closedListener(e, $form) {
-  e.stopPropagation();
-  $($form).on('click', () => {
-    menu(vis, $form);
-    vis = !vis;
-    $form.off('click');
-  })
-}
-
-function manPane(vis) {
-  const $menu = $('#menu');
-
-  if (vis) {
-    $menu.animate({'height': '8vh'});
-    $menu.removeClass('init');
-  }
-  else {
-    $menu.animate({'height': '80vh'});
-  }
-};
-
-function manForm(vis, form) {
-  const $inputs = $('input'),
-        $bars = $('.bar'),
-        $submit = $('#click');
-
-  if (vis) {
-    let left_c = $('body').width()/2 - (form.width()/5.35714286)/2;
-    form.animate({left: ~left_c });
-  } else {
-    form.animate({left: 0});
+  function closedListener(e, $form) {
+    e.stopPropagation();
+    $($form).on('click', () => {
+      menu(vis, $form);
+      vis = !vis;
+      $form.off('click');
+    })
   }
 
-  for (var i = 0; i < $inputs.length; i++) {
-    vis ?
-    $inputs.eq(i).slideUp():
-    $inputs.eq(i).slideDown();
-    vis ?
-    $bars.eq(i).animate({'width':'2.8vw'}):
-    $bars.eq(i).animate({'width': '15vw'});
-  }
+  function manPane(vis) {
+    const $menu = $('#menu');
 
-  vis ? $submit.slideUp() : $submit.slideDown();
+    if (vis) {
+      $menu.animate({'height': '8vh'});
+      $menu.removeClass('init');
+    }
+    else {
+      $menu.animate({'height': '80vh'});
+    }
 
-};
+    if (vis) {
+      $('body').append('<div id="new">New Location</div>');
+      $('#new').css('top', '8vh');
+      // $menu.hover(() => {
+      //   $('#new').slideDown();
+      // })
+    } else {
+      $('#new').remove();
+    }
+  };
+
+  function manForm(vis, form) {
+    const $inputs = $('input'),
+          $bars = $('.bar'),
+          $submit = $('#click');
+
+    if (vis) {
+      let left_c = 50 - (((form.width()/5.35714286)/$('body').width()) * 100);
+      form.animate({left: ~left_c + 'vw' });
+    } else {
+      form.animate({left: 0});
+    }
+
+    for (var i = 0; i < $inputs.length; i++) {
+      vis ?
+      $inputs.eq(i).slideUp():
+      $inputs.eq(i).slideDown();
+      vis ?
+      $bars.eq(i).animate({'width':'2.8vw'}):
+      $bars.eq(i).animate({'width': '15vw'});
+    }
+
+    vis ? $submit.slideUp() : $submit.slideDown();
+
+  };
+})();
