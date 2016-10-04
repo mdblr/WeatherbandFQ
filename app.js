0a1a6469aceb34c5c198d6aae859e767c42b7c83
Max Dobler <mdblr@outlook.com>
2016-10-04 11:31:27 -0700

    remove old files

--- app.js
+++ app.js
@@ -1,155 +1,96 @@
-var $temp = $('#temp');
-var $input = $('#input');
-var today = moment().format("[today] h:mm a");
-var apiVal;
-var $weatherDeets = $('#weatherDetails');
-var $back = $('#back');
-var temp, summary;
-var $reset = $('#reset');
+(() => {
 
 
-$('#click').on("click", function(){
-  apiVal = $('#state').val() + "/" + $('#city').val() + "/" + $('#zip').val() + ".json";
-  $('#slideshow').removeClass('landingBlur');
-  // $('#creds').fadeOut();
-  weatherUnderG(apiVal);
-});
-
-$reset.on("click", function(){
+  loadBackgrounds();
+  mtDropdown();
+  submit();
   reset();
-});
-
-// background rotation
-function backgrounds() {
-	$('#slideshow').cycle({
-	fx: 'fade',
-	pager: '#smallnav',
-	pause:   0,
-	speed: 3500,
-	timeout:  3500
-	});
-}
 
 
 
-$('document').ready(function(){
-  backgrounds();
+  function submit() {
+    const $setLoc = $('#submit'),
+          $inputs = $('input'),
+          $err = $('#error');
+    let eventDisable = false;
 
-  var link = document.querySelector('link[rel="import"]');
-  link.addEventListener('load', function(e) {
-    var importedDoc = link.import;
-    // importedDoc points to the document under component.html
-  });
+    $setLoc.on('click keydown', e => {
 
-  console.log(link.import);
-  console.log($('#locate').text());
-});
+      if (!(e.which === 1 || e.which === 13) || eventDisable) return;
+      else eventDisable = true;
+      if ($err.is(':visible')) $err.hide();
 
+      let formInput, locString,
+          coordsAJAX, coords,
+          weatherAJAX, weather;
 
-// WeatherUnderground API
-function weatherUnderG(apiVal) {
-  $.ajax({
-    method: "GET",
-    dataType: 'jsonp',
-    url: "http://api.wunderground.com/api/a23692177cc6bae1/conditions/q/" + apiVal
-  })
-  .done(function(info) {
+      formInput = form.getValues($inputs);
+      locString = forecast.stringLocParams(formInput);
+      coordsAJAX = forecast.coordsAPI(locString);
 
-    var userCoords = (function(){
-      var wugSplit = (info.current_observation.ob_url).split("=");
-      var forecastAPI = "https://api.forecast.io/forecast/ec4ea27eb974f4bdcd500583b2c49367/" + wugSplit[1];
-      return forecastAPI;
-    })(info.current_observation.ob_url);
-    rowWeather(userCoords);
-    displayForecast(info);
-   })
-  .fail(function(err){
-    console.log("FAIL");
-    console.log(err);
-  });
-}
+      coordsAJAX
+        .then( res => {
+          forecast.errorHandling(res);
+          coords = forecast.splitCoordsRes(res);
+          weatherAJAX = forecast.weatherAPI(coords);
 
-//forecastIO call
-function rowWeather(string){
-  $.ajax({
-    method: "GET",
-    dataType: "jsonp",
-    url: string
-  })
-  .done(function(info) {
-    console.log(info);
-    bgWeatherFX(info.currently.icon);
-    summary = info.daily.summary;
-    weatherIcon(info.currently.icon);
-  })
-  .fail(function(err){
-    console.log(err);
-  });
-}
-
-// User's Weather
-function displayForecast(info) {
-    temp =  info.current_observation.temp_f + '\u00B0';
-    $.when($input.fadeOut(320)).then(function(){
-      $('#background').addClass('bgFade2');
-      $('#background').fadeIn();
-
+          return weatherAJAX
+          .then( res => {
+            weather = res;
+            dash.viewOne(forecast.currentTempF(weather));
+            dash.stageSubViews(forecast.dailySummary(weather));
+            menu.close();
+            return weather;
+          })
+          .catch( err => {
+            throw new Error('Problem with weather service.');
+          });
+        })
+        .catch( err => {
+          form.showErr($err, err);
+          $err.show();
+        })
+        .then( weather => {
+          eventDisable = false;
+        });
     });
-}
-
-function bgWeatherFX(conditions) {
-  if (conditions === "rain") {
-    $('#slideshow div').addClass('rain');
-  } else if (conditions === "clear-day"){
-    $('#slideshow div').addClass('clear');
-  } else if (conditions === "snow"){
-    $('#slideshow div').addClass('snow');
-  } else if (conditions === "fog"){
-    $('#slideshow div').addClass('fog');
-  } else if (conditions === "partly-cloudy-day") {
-    $('#slideshow div').addClass('partlyCloudy');
-  } else if (conditions === "cloudy"){
-    $('#slideshow div').addClass('cloudy');
   }
-}
 
-function weatherIcon(conditions) {
 
-  if (conditions === "clear-day") {
-    $temp.prepend('<i class="wi wi-day-sunny"></i>');
-  } else if (conditions === "clear-night"){
-    $temp.prepend('<i class="wi wi-night-clear"></i>');
-  } else if (conditions === "rain"){
-    $temp.prepend('<i class="wi wi-rain"></i>');
-  } else if (conditions === "snow"){
-    $temp.prepend('<i class="wi wi-snow"></i>');
-  } else if (conditions === "sleet"){
-    $temp.prepend('<i class="wi wi-sleet"></i>');
-  } else if (conditions === "wind"){
-    $temp.prepend('<i class="wi wi-strong-wind"></i>');
-  } else if (conditions === "fog"){
-    $temp.prepend('<i class="wi wi-fog"></i>');
-  } else if (conditions === "partly-cloudy-day"){
-    $temp.prepend('<i class="wi wi-day-cloudy"></i>');
-  } else if (conditions === "partly-cloudy-night"){
-    $temp.prepend('<i class="wi wi-night-partly-cloudy"></i>');
-  } else if (conditions === "cloudy"){
-    $temp.prepend('<i class="wi wi-cloudy"></i>');
-  } else {
-    $temp.prepend('<i class="wi wi-thermometer-exterior"></i>');
+  function reset() {
+    const $resetLoc = $('#dropdown');
+
+    $resetLoc.click(() => {
+      menu.open();
+    });
   }
-  $temp.append(temp);
-  $temp.fadeIn(400);
-  $('summary').append(summary);
-  $reset.fadeIn(200);
-  $('footer').fadeIn(400);
 
-}
 
-function reset(){
-  $('footer').toggle();
-  $temp.empty();
-  $('summary').empty();
-  $('#background').removeClass('bgFade2');
-  $input.slideDown();
-}
+  function mtDropdown() {
+    const $newLocation = $('#dropdown'),
+          $submit = $('#submit'),
+          $pane = $('#pane'),
+          $menu = $('#menu');
+
+    $menu.hover(() => {
+      if ($submit.is(':visible')) return;
+      $newLocation.show();
+    });
+
+    $pane.mouseleave(() => {
+      $newLocation.hide();
+    })
+  }
+
+
+  function loadBackgrounds() {
+    $('#background-carousel').cycle({
+      fx: 'fade',
+      pager: '#smallnav',
+      pause:   0,
+      speed: 3500,
+      timeout:  3500
+    });
+  }
+
+})();
